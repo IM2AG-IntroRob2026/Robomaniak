b93f46be88b553c596cca10a8cc4cc5ec31a3121
antoine <antoine.patron@etu.univ-littoral.fr>
2026-04-01 21:09:40 +0200

    feat(tests): add unit test for YoloDetector provider name

---
+++ src/robot_vision/test/test_yolo_detector.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "robot_vision/yolo_detector.hpp"
+
+using namespace robot_vision;
+
+TEST(YoloDetectorTest, TestProviderName)
+{
+    std::string_view providerName("CPU");
+
+    YoloDetector detector("yolov8s.onnx", false);
+    EXPECT_EQ(detector.providerName(), providerName);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
